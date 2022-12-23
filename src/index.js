3794c11f6c892503d7f0e7f8762fa3223250b0a2
wonecode <lochlainn.gadault@gmail.com>
2022-12-23 16:17:29 +0100

    feat: automatic message every day

--- src/index.js
+++ src/index.js
@@ -60,6 +60,16 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 client.once(Events.ClientReady, (c) => {
   client.user.setPresence({ activities: [{ name: 'New World' }] });
 
+  const whatsup = client.channels.cache.get(`1054714484939305000`);
+
+  setInterval(async () => {
+    if (moment().locale('fr').format('HH:mm') === '16:30') {
+      await whatsup.send(
+        "Salut tout le monde ! Quoi de neuf de prévu sur le jeu aujourd'hui ? *(c'est pas parce que je suis un bot que ça t'empêche de répondre hein :wink:)* @everyone"
+      );
+    }
+  }, 10000);
+
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
@@ -91,7 +101,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
       const response = Number(interaction.fields.getTextInputValue('balanceInput'));
       const updatedDate = moment().utc().format();
 
-      console.log(response)
+      console.log(response);
 
       const { error } = await supabase
         .from('balance')
