d878212e1a716fa796703eaf976a2ed00fb6a43b
wonecode <lochlainn.gadault@gmail.com>
2022-12-23 16:37:29 +0100

    fix: automatic message

--- src/index.js
+++ src/index.js
@@ -63,12 +63,12 @@ client.once(Events.ClientReady, (c) => {
   const whatsup = client.channels.cache.get(`1054714484939305000`);
 
   setInterval(async () => {
-    if (moment().locale('fr').format('HH:mm') === '16:30') {
+    if (moment().locale('fr').format('HH:mm:ss') === '16:30:00') {
       await whatsup.send(
         "Salut tout le monde ! Quoi de neuf de prévu sur le jeu aujourd'hui ? *(c'est pas parce que je suis un bot que ça t'empêche de répondre hein :wink:)* @everyone"
       );
     }
-  }, 10000);
+  }, 1000);
 
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
